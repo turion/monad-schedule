28ab7943e4091e92897526b9611ad1ae6281e199
Manuel Bärenz <m.baerenz@sonnen.de>
2022-05-25 12:51:19 +0200

    Add test suite

---
+++ test/Main.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE RecordWildCards #-}
+-- base
+import Control.Arrow
+import Control.Monad
+import Data.Functor.Identity
+import Data.List.NonEmpty
+
+-- test-framework
+import Test.Framework
+
+-- test-framework-hunit
+import Test.Framework.Providers.HUnit
+
+-- HUnit
+import Test.HUnit hiding (Test)
+
+-- test-framework-quickcheck2
+import Test.Framework.Providers.QuickCheck2
+
+-- QuickCheck
+import Test.QuickCheck
+
+-- monad-schedule (test)
+import qualified Trans
+import qualified Yield
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: [Test]
+tests =
+  [ Trans.tests
+  , Yield.tests
+  ]
